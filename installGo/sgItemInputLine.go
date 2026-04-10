00d94a9463e9decdae06f6bdf2a37d50b7ad8f4e
asibilev <usiksan32@yandex.ru>
2026-04-10 16:08:03 +0300

    Try fix windows keyboard issue

--- installGo/sgItemInputLine.go
+++ installGo/sgItemInputLine.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "unicode"
+  "strconv"
   )
 
 
@@ -226,6 +227,8 @@ func (in *SgItemInputLine) MouseClick(localX, localY int) bool {
 
 // keyDown обработка нажатий клавиш
 func (in *SgItemInputLine) KeyDown(code int) {
+in.TextSet( strconv.Itoa(code) )
+    SgWinRepaint()
   if in.Focus {
     switch code {
       case KeyLeft:
