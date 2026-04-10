b2bde7d461289beab50ea3880b1f4ac5201971ca
asibilev <usiksan32@yandex.ru>
2026-04-10 14:48:43 +0300

    Fix: wrong ip to server

--- src/SdConfig.h
+++ src/SdConfig.h
@@ -113,8 +113,8 @@ History
 #define SD_GET_OBJECT_MAX_FIND_LIST  300
 
 //Default IP for global storage and private cloud
-#define SD_DEFAULT_GLOBAL_STORAGE_IP "178.212.013.179"
-#define SD_DEFAULT_PRIVATE_CLOUD_IP  "178.212.013.179"
+#define SD_DEFAULT_GLOBAL_STORAGE_IP "178.212.13.179"
+#define SD_DEFAULT_PRIVATE_CLOUD_IP  "178.212.13.179"
 
 //Settings key names
 #define SDK_WMAIN_MAX                "WMainMax"
